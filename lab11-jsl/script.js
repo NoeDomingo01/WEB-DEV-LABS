f9955e61c2dfd4e9d9857890e6330252649b3e21
NoeDomingo01 <domingonoedabuet04@gmail.com>
2025-10-20 20:44:19 +0800

    lab11 |  Basic JS Syntax

---
+++ lab11-jsl/script.js
@@ -0,0 +1,11 @@
+// Variable declaration using let
+let name = "Noe";
+console.log("Hello, " + name + "!");
+
+// Math operation using two variables
+let num1 = 10;
+let num2 = 5;
+let sum = num1 + num2;
+
+// Output results to console
+console.log("The sum of " + num1 + " and " + num2 + " is " + sum);
